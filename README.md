ae76d469c8fc8ac2c28caefd817db3dff6a514f8
chrisittner <christian.ittner@alasco.de>
2024-10-06 18:29:41 +0200

    improve cmd helptext & add examples

--- README.md
+++ README.md
@@ -4,12 +4,12 @@
 
 List owners of files based on the [CODEOWNERS](https://docs.github.com/en/repositories/managing-your-repositorys-settings-and-features/customizing-your-repository/about-code-owners) file of the current repository.
 
-```
+```bash
 > git codeowners src/main.rs
 src/main.rs                              @weyland
 ```
 
-```
+```bash
 > git codeowners `git ls-files`
 .gitignore                               (unowned)
 Cargo.lock                               @weyland
@@ -19,14 +19,14 @@ README.md                                @weyland
 src/main.rs                              @weyland
 ```
 
-```
+```bash
 # Oops, did the last commit add unowned files?
 > git diff --name-only --diff-filter=A | git codeowners
 .gitignore                               @bob
 LICENSE                                  (unowned)
 ```
 
-```
+```bash
 # I need stats about my repo
 > echo `git ls-files | git codeowners | grep "(unowned)"| wc -l` out of `git ls-files | git codeowners | wc -l` files in this repository do not have a corresponding CODEOWNERS entry
 2 out of 6 files in this repository do not have a CODEOWNERS entry
@@ -37,54 +37,53 @@ LICENSE                                  (unowned)
 - via [pip](https://pypi.org/project/git-codeowners/): `pip install git-codeowners`
 - via [Cargo](https://crates.io/crates/git-owners): `cargo install git-owners`
 
-  Note: The Cargo crate installs an equivalent `git-owners` command in addition to `git-codeowners`, for backwards compatibility.
+  *The Cargo crate installs an equivalent `git-owners` command in addition to `git-codeowners`, for backwards compatibility.*
 
 
 ## Usage
 
 - Show owner of a file
 
-  ```
+  ```bash
   git codeowners some/file
   ```
 
 - Show owners for a list of files
 
-  ```
+  ```bash
   git codeowners some/file some/other/file
   ```
 
-> [!NOTE]
-> Each path should be relative to the git repository root -- this makes it easy to combine with other git commands.
+Each path should be relative to the git repository root. This makes it easy to combine with other git commands:
 
 - Show owners for every tracked file in the repository
 
-  ```
+  ```bash
   git ls-files | git codeowners
   ```
 
 - Show owners for files modified in last five commits
 
-  ```
+  ```bash
   git diff --name-only HEAD~5 HEAD | git codeowners
   ```
 
 - Congratulate the user if the current changeset does not add files without owner
 
-  ```
+  ```bash
   git diff --diff-filter=ACR --name-only | git codeowners && echo "Great job! No unowned files added!"
   ```
 
 - Get an overview of your CODEOWNERS coverage
-  ```
+  ```bash
   echo `git ls-files | git codeowners | grep "(unowned)"| wc -l` out of `git ls-files | git codeowners | wc -l` files in this repository do not have a CODEOWNERS entry
   ```
 
 ## Features
 
-- Detects the right CODEOWNERS file of the current git repository
+- Detects the right CODEOWNERS file of the current git repository (`CODEOWNERS`, `docs/CODEOWNERS`, or `.github/CODEOWNERS`)
+- Works well as a pre-commit hook; Returns a non-zero exit code if unowned files are found
 - Is composable & scripting friendly
-- Works well as a pre-commit hook
 - Is fast & written in Rust
 
 ## Issues & Contributing
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,96 @@
 //! A git subcommand to query and validate CODEOWNERS.
-//! See [https://github.com/chrisittner/git-owners](https://github.com/chrisittner/git-owners)
+//! See [https://github.com/chrisittner/git-codeowners](https://github.com/chrisittner/git-codeowners)
 
 extern crate codeowners;
 use atty::Stream;
-use clap::Parser;
+use clap::{CommandFactory, Parser};
+use clap_help::Printer;
 use std::io::{self, BufRead};
 use std::path;
 use std::process;
 
-/// git-owners - List owners of files
+/// git-codeowners - List owners of files
 /// based on the CODEOWNERS file of the current repository
 #[derive(Parser, Debug)]
-#[clap(author, version, about, long_about = None)]
+#[clap(author, version, about, long_about = None, disable_help_flag = true)]
 struct Args {
     /// One or more file paths for which to check ownership.
     /// Can also be provided via pipe/stdin.
     /// Each path should be relative to the git repository root
-    /// -- this makes it easy to do e.g. `git ls-files | git owners`
-    #[clap(required(true))]
+    /// -- this makes it easy to do e.g. `git ls-files | git codeowners`
+    #[clap()]
     paths: Vec<String>,
+
+    /// Print help
+    #[arg(short, long)]
+    help: bool,
 }
 
+static INTRO: &str = "List owners of files based on the CODEOWNERS file of the current repository";
+static USAGE: &str = "
+**Usage: ** `git codeowners [options]${positional-args}`
+";
+
+pub struct Example {
+    pub title: &'static str,
+    pub cmd: &'static str,
+}
+
+impl Example {
+    pub const fn new(title: &'static str, cmd: &'static str) -> Self {
+        Self { title, cmd }
+    }
+}
+
+pub static EXAMPLES: &[Example] = &[
+    Example::new("Show owners for every tracked file in the repository", "git ls-files | git codeowners"),
+    Example::new(
+        "Show owners for files modified in last five commits",
+        "git diff --name-only HEAD~5 HEAD | git codeowners"
+    ),
+    Example::new(
+        "Congratulate the user if the current changeset does not add files without owner",
+        "git diff --diff-filter=ACR --name-only | git codeowners && echo \"Great job! No unowned files added!\""
+    ),
+    Example::new(
+        "Get an overview of CODEOWNERS coverage",
+        "echo `git ls-files | git codeowners | grep \"(unowned)\"| wc -l` out of `git ls-files | wc -l` files do not have a CODEOWNERS entry"
+    ),
+];
+
+static EXAMPLES_TEMPLATE: &str = "
+**Examples:**
+
+${examples
+**${example-number})** ${example-title}:
+`${example-cmd}`
+
+}
+";
+
 fn main() {
+    let args = Args::parse();
+    if args.help || args.paths.is_empty() {
+        let mut printer = Printer::new(Args::command())
+            .with("introduction", INTRO)
+            .with("usage", USAGE)
+            .without("title")
+            .without("author");
+
+        printer.template_keys_mut().push("examples");
+        printer.set_template("examples", EXAMPLES_TEMPLATE);
+        for (i, example) in EXAMPLES.iter().enumerate() {
+            printer
+                .expander_mut()
+                .sub("examples")
+                .set("example-number", i + 1)
+                .set("example-title", example.title)
+                .set("example-cmd", example.cmd);
+        }
+        printer.print_help();
+        return;
+    }
+
     if data_was_piped_in() {
         let stdin = io::stdin();
         let lines_from_stdin = stdin.lock().lines();
